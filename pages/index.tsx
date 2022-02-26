baf8d01a95516bafa7f64982c0525ac6720165fb
Brian3647 <victoris3647@gmail.com>
2022-02-26 09:12:59 +0100

    :sparkles: Tailwindcss + prettier rules

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,26 +4,26 @@ import { Button } from "antd";
 import { Styles } from "./types.d";
 
 const Home = () => {
-  const { data: session } = useSession();
+	const { data: session } = useSession();
 
-  const style: Styles = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "100vh",
-  };
+	const style: Styles = {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "center",
+		height: "100vh",
+	};
 
-  if (!session) {
-    return <div style={style}>Sign in to see your profile!</div>;
-  }
+	if (!session) {
+		return <div style={style}>Sign in to see your profile!</div>;
+	}
 
-  return (
-    <div style={style}>
-      Hello fellow new user!
-      <Button href="/post">Post!</Button>
-    </div>
-  );
+	return (
+		<div style={style}>
+			Hello fellow new user!
+			<Button href="/post">Post!</Button>
+		</div>
+	);
 };
 
 export default Home;
