1e7b2ffbccf77115b47445024c0f8729b62bdcb4
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-26 11:01:50 -0500

    :sparkles: Feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,57 @@
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 
+import Image from "next/image";
+
 import { Styles } from "./types.d";
 
+import { IPost } from "../lib/db/models";
+import { useEffect, useState } from "react";
+
 const Home = () => {
-	const { data: session } = useSession();
+	const { data } = useSession();
+	const [post, setPost] = useState<IPost[]>();
+
+	useEffect(() => {
+		fetch("/api/allPosts")
+			.then((res) => res.json())
+			.then((data) => setPost(data));
+	}, []);
 
 	const style: Styles = {
 		display: "flex",
 		flexDirection: "column",
 		alignItems: "center",
-		justifyContent: "center",
-		height: "100vh",
 	};
 
-	if (!session) {
-		return <div style={style}>Sign in to see your profile!</div>;
-	}
-
 	return (
-		<div style={style}>
-			Hello fellow new user!
-			<Link href="/post">Post!</Link>
-		</div>
+		<>
+			{data ? (
+				<div style={style}>
+					Hello fellow {data.user?.name ?? "new"} user!
+					<Link href="/post">Post!</Link>
+				</div>
+			) : (
+				<div style={style}>Sign in to see your profile!</div>
+			)}
+
+			<h1>Latest Posts</h1>
+
+			{post &&
+				post.map((post, index) => (
+					<div key={index}>
+						<p> A dish of {post.user}</p>
+						<h2>{post.title}</h2>
+						<p>{post.description}</p>
+						<Image
+							src={post.imageRawPath}
+							height={300}
+							width={300}
+							alt={post.description}
+						/>
+					</div>
+				))}
+		</>
 	);
 };
 
