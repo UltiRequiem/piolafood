3493aace37a3367a93783db1a55427c26a1e397d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-26 03:26:33 -0500

    :bug: Fix Bug

--- lib/images.ts
+++ lib/images.ts
@@ -1,13 +1,11 @@
 import { TixteClient } from "@ultirequiem/tixte";
 
-import { randomItem } from "./others";
-
 const OUR_DOMAINS = ["likes.cash", "wants.solutions", "is-from.space"];
 
 if (!process.env.TIXTE_CLIENT_KEY) {
-  throw new Error("TIXTE_CLIENT_KEY is not set");
+	throw new Error("TIXTE_CLIENT_KEY is not set");
 }
 
 export const client = new TixteClient(process.env.TIXTE_CLIENT_KEY, {
-  defaultURL: `piolafood.${randomItem(OUR_DOMAINS)}`,
+	defaultURL: `piolafood.${OUR_DOMAINS[1]}`,
 });
