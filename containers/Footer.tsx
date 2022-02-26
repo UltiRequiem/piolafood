2135660c3a0d64995b7cf7c3661a49a3c65c887d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-26 11:39:17 -0500

    :recycle: Footer Component

--- containers/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer style={{ textAlign: "center" }}>
-      Made with 💖 by <a href="https://github.com/folkip">Folkip</a>
-    </footer>
-  );
-};
---
+++ containers/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./footer.module.scss";
+
+export const Footer = () => {
+	return (
+		<footer className={styles.footer}>
+			Made with 💖 by <a href="https://github.com/folkip">Folkip</a>
+		</footer>
+	);
+};
