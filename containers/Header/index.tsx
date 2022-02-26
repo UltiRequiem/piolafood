714d8bc2071f6165f21eedd5148daacd5144c6b3
Brian3647 <victoris3647@gmail.com>
2022-02-26 17:29:12 +0100

    :sparkles: Add avatar

--- containers/Header/index.tsx
+++ containers/Header/index.tsx
@@ -1,15 +1,33 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
 
 import styles from "./index.module.scss";
 
 export const Header = () => {
 	const { data } = useSession();
 
+	console.log(data);
+
 	return (
 		<div className={styles.header}>
 			<h1 className={styles.title}>Piolafood</h1>
 			{data ? (
 				<>
+					<div className={styles.user}>
+						<Image
+							src={data.user!.image as string}
+							alt="Profile picture"
+							width={40}
+							height={40}
+							className={styles.img}
+						></Image>
+
+						<div className={styles.username}>
+							<Link href={"/" + data.user!.email}>{data.user?.name}</Link>
+						</div>
+					</div>
+
 					<button className={styles.btn} onClick={() => signOut()}>
 						Sign out
 					</button>
