79c50d5f94c7baa8a9cdc3db020a490dcc6cca5e
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-25 22:48:23 -0500

    :bug: Fix Bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,8 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+  _req: NextApiRequest,
+  res: NextApiResponse<{ name: string }>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
