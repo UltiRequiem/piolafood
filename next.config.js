9d7ff27482e4472cb409db4fa0e99d80438ecf84
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-26 11:47:10 -0500

    :bug: re add "piolafood.is-from.space"

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ["piolafood.wants.solutions", "avatars.githubusercontent.com"],
+		domains: [
+			"piolafood.wants.solutions",
+			"avatars.githubusercontent.com",
+			"piolafood.is-from.space",
+		],
 	},
 };
 
